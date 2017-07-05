bcec61351cf0cf1b8b92c2f42fb3cc6c90796140
[01;31m[K"name"[m[K: "Nick <nsksir@gmail.com>
2017-07-05 16:53:13 -0700

    update types for redux actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,7 +5,13 @@ export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER = 'login_user';
 
-// employee const's
-export const EMPLOYEE_UPDATE = 'employee_update';
-export const EMPLOYEE_CREATE = 'employee_create';
-export const EMPLOYEE_FETCH_SUCCESS = 'employee_fetch_success';
+// plans const's
+export const PLANS_FETCH = 'plans_fetch';
+
+// plan
+export const PLAN_CREATE = 'plan_create';
+export const PLAN_UPDATE = 'plan_update';
+export const PLAN_LOCALS_FETCH = 'plan_locals_fetch';
+
+// local
+export const LOCAL_FETCH = 'plan_locals_fetch';
