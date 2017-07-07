a67f6637f568f4b40f093bdb6c7e28c360c7ad80
[01;31m[K"name"[m[K: "Nick <nsksir@gmail.com>
2017-07-06 17:04:04 -0700

    change plans list to call ruby back end DB instead of firebase

--- src/actions/PlanActions.js
+++ src/actions/PlanActions.js
@@ -1,4 +1,3 @@
-import firebase from 'firebase';
 import axios from 'axios';
 import {Actions} from 'react-native-router-flux'
 import {
@@ -14,23 +13,21 @@ export const planUpdate = ({prop, value}) => {
   };
 };
 export const planCreate = ({ title, city, district}) => {
-  // const { currentUser } = firebase.auth();
   return (dispatch) => {
     var user_id = 1;
      axios.post('http://localhost:3000/plans', { params: { user_id: user_id, title: title, city: city, district: district}
     }).then((response) => {
       dispatch({ type: PLAN_CREATE, payload: response.data});
-      Actions.plan();
+     Actions.plan();
     });
   };
 };
 export const plansFetch = () => {
-  const { currentUser } = firebase.auth();
-  console.log()
+
   return (dispatch) => {
-    firebase.database().ref(`/users/${currentUser.uid}/plans`)
-      .on('value', snapshot => {
-        dispatch({ type: PLANS_FETCH, payload: snapshot.val() });
-      });
+  var user_id = 1;
+  axios.get(`http://localhost:3000/users/${user_id}`).then((response) => {
+    dispatch({ type: PLANS_FETCH, payload: response.data});
+  });
   };
 };
--- src/reducers/PlanFormReducer.js
+++ src/reducers/PlanFormReducer.js
@@ -15,8 +15,7 @@ export default (state = INITIAL_STATE, action) => {
     case PLAN_UPDATE:
       return { ...state, [action.payload.prop]: action.payload.value};
     case PLAN_CREATE:
-      console.log("ID: ", action.payload.id)
-      return { ...state, plan_id:  action.payload.id};
+    return { ...state, plan_id:  action.payload.id};
     default:
       return state;
   }
