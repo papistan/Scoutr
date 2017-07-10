1c64116a23f20be13c9f24d362b0ad8d198b079a
[01;31m[K"name"[m[K: "Nick <nsksir@gmail.com>
2017-07-09 19:16:24 -0700

    got the liks for each plan and the return to show page to work properly

--- src/actions/PlanLocalesActions.js
+++ src/actions/PlanLocalesActions.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import {Actions} from 'react-native-router-flux'
 import {
-  PLAN_LOCALES_FETCH
+  PLAN_LOCALES_FETCH,
+  CURRENT_PLAN
 } from './types';
 
-export const planLocalesFetch = () => {
+export const planLocalesFetch = (plan) => {
   return (dispatch, state) => {
-  debugger
+  if (plan) {
+     state().planForm.currentPlan = plan;
+  }
   var plan_id = state().planForm.currentPlan.id;
   axios.get(`http://localhost:3000/plans/${plan_id}`).then((response) => {
     dispatch({type: PLAN_LOCALES_FETCH, payload: response.data})
