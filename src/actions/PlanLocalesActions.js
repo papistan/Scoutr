27415b365bb9d542e095be07eef993a7873f84de
[01;31m[K"name"[m[K: "Nick <nsksir@gmail.com>
2017-07-11 17:26:52 -0700

    remove debugger

--- src/actions/PlanLocalesActions.js
+++ src/actions/PlanLocalesActions.js
@@ -4,25 +4,22 @@ import {
   PLAN_LOCALES_FETCH
 } from './types';
 
-        export const planLocalesFetch = (plan) => {
-          return (dispatch, state) => {
-          if (plan) {
-             state().planForm.currentPlan = plan;
-          }
-          var plan_id = state().planForm.currentPlan.id;
-          axios.get(`http://localhost:3000/plans/${plan_id}`).then((response) => {
-            dispatch({type: PLAN_LOCALES_FETCH, payload: response.data})
-          });
-          };
-        };
+  export const planLocalesFetch = (plan) => {
+    return (dispatch, state) => {
+    if (plan) {
+       state().planForm.currentPlan = plan;
+    }
+    var plan_id = state().planForm.currentPlan.id;
+    axios.get(`http://localhost:3000/plans/${plan_id}`).then((response) => {
+      dispatch({type: PLAN_LOCALES_FETCH, payload: response.data})
+    });
+    };
+  };
 export const localeDelete = (locale) => {
   return (dispatch) => {
-  debugger
   axios.delete(`http://localhost:3000/locales/${locale.id}`, { params: {
       locale_id: locale.id }})
-  debugger
   axios.get(`http://localhost:3000/plans/${plan_id}`).then((response) => {
-  debugger
     dispatch({type: PLAN_LOCALES_FETCH, payload: response.data})
   });
   };
